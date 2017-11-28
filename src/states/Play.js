7979d7430d0a1ca1a1c26079b0debef6901825c4
Stephan Max <stephan.max@gmail.com>
2017-11-29 00:47:03 +0100

    Moving platforms, death, had to hack with velocity a little bit

--- src/states/Play.js
+++ src/states/Play.js
@@ -1,34 +1,62 @@
 import Phaser from 'phaser'
 
 export default class extends Phaser.State {
-  create () {
-    const bmd = game.add.bitmapData(game.width, game.height * .3)
+  init() {
+    this.totalGroundWidth = 0
+  }
+
+  makeGround() {
+    const width = this.ground.length === 0 ? game.width * 2 : game.width * game.rnd.realInRange(.3, .7)
+    const gap = this.ground.length === 0 ? 0 : game.width * game.rnd.realInRange(.2, .4)
+    this.totalGroundWidth += gap
+    const height = game.height * game.rnd.realInRange(.2, .4)
+    const bmd = game.add.bitmapData(width, height)
     bmd.ctx.beginPath()
-    bmd.ctx.rect(0, 0, game.width, game.height * .3)
-    bmd.ctx.fillStyle = '#c88fe4'
+    bmd.ctx.rect(0, 0, width, height)
+    bmd.ctx.fillStyle = game.rnd.pick(['#2ecc71', '#3498db', '#9b59b6', '#f1c40f', '#e67e22', '#e74c3c', '#95a5a6', '#34495e', '#1abc9c'])
     bmd.ctx.fill()
+
+    const ground = this.ground.create(this.totalGroundWidth, game.height - height, bmd)
+    this.totalGroundWidth += width
+    ground.body.velocity.x = -game.width * .5
+    ground.body.immovable = true
+    ground.checkWorldBounds = true
+    ground.__gap = gap
     
-    this.ground = game.add.sprite(0, game.height * .7, bmd)
-    
-    game.physics.enable(this.ground, Phaser.Physics.ARCADE)
-    this.ground.body.immovable = true
+    ground.events.onOutOfBounds.add((ground) => {
+      if (ground.x < 0) {
+        this.totalGroundWidth -= ground.width
+        this.totalGroundWidth -= ground.__gap
+        ground.destroy()
+      }
+    }, this)
+  }
+
+  create () {
+    this.ground = game.add.group()
+    this.ground.enableBody = true
+    this.ground.physicsBodyType = Phaser.Physics.ARCADE
+
+    this.ground
 
     const scaleFactor = (game.width * .2) / 80
 
-    this.samurai = game.add.sprite(game.width * .1, game.height * .7 - (scaleFactor * 58), 'samurai')
+    this.samurai = game.add.sprite(game.width * .1, game.height * .2, 'samurai')
+    console.log(this.samurai.x)
     this.samurai.scale.setTo(scaleFactor, scaleFactor)
     this.samurai.smoothed = false
 
-    this.jumping = false
+    this.jumping = true
     this.doublejump = true
     this.canAttack = true
+    this.dead = false
 
     this.samurai.animations.add('run', [0, 1, 2, 3, 4, 5], 18, true)
     this.samurai.animations.add('attack', [7, 10, 11, 12, 13], 32)
     this.samurai.animations.add('doublejump', [16, 14], 10)
 
     game.physics.enable(this.samurai, Phaser.Physics.ARCADE)
-    this.samurai.body.gravity.y = game.height * 1.7
+    this.samurai.body.gravity.y = game.height * 2
     this.samurai.body.setSize(50, 50, 0, 0)
 
     this.attackBtn = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
@@ -50,8 +78,25 @@ export default class extends Phaser.State {
   }
 
   update() {
+    if (this.ground.length < 5) {
+      this.makeGround()
+    }
+
     game.physics.arcade.collide(this.samurai, this.ground, () => {
-      if (this.jumping) {
+      if (this.samurai.x >= game.width * .1) {
+        this.samurai.body.velocity.x = game.width * .5
+      }
+
+      if (this.samurai.body.velocity.y !== 0) {
+        this.dead = true
+        this.samurai.frame = 18
+        game.input.onTap.removeAll()
+        game.input.keyboard.stop()
+        game.paused = true
+      }
+
+      if (this.jumping && !this.dead) {
+        this.samurai.body.velocity.x = game.width * .52
         this.samurai.animations.play('run')
         this.jumping = false
         this.doublejump = true
@@ -59,8 +104,14 @@ export default class extends Phaser.State {
       }
     })
 
-    if (this.samurai.body.velocity.y > 0) {
+    if (this.samurai.body.velocity.y > 0 && !this.dead) {
       this.samurai.frame = 15
+      this.samurai.body.velocity.x = 0
+    }
+
+    if (this.samurai.y > game.height) {
+      this.dead = true
+      game.paused = true
     }
 
     document.getElementById('fps').innerHTML = game.time.fps
@@ -90,7 +141,8 @@ export default class extends Phaser.State {
       this.canAttack = false
     }
     this.jumping = true
-    this.samurai.body.velocity.y = -game.height * .9
+    this.samurai.body.velocity.y = -game.height * .7
+    this.samurai.body.velocity.x = 0
     this.samurai.animations.stop()
   }
 }
